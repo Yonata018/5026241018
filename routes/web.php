809c257e497ae52e55eda653d23682414214dde7
Yonata018 <pandemy26@gmail.com>
2026-05-11 11:47:16 +0700

    pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiDBController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,6 +24,19 @@ Route::get('blog', function () {
 Route::get('pert5', function () {
 	return view('pertemuan5');
 });
+
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('biodata', [DosenController::class, 'biodata']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
 
 Route::get('pert1', function () {
 	return view('intro');
@@ -49,3 +65,14 @@ Route::get('linktree', function () {
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+Route::get('/pegawainama/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+//blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+// CRUD Tabel Pegawai
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
